20bb62a60a8e36cb249b18a9c5b3865e2be11434
devon <adam.boczekwork@gmail.com>
2023-05-22 21:34:33 +0100

    Add new basket app + adding 2 more fields into items model.py

--- items/models.py
+++ items/models.py
@@ -29,6 +29,8 @@ class Item(models.Model):
     product_model = models.CharField(max_length=254, null=True, blank=True)
     product_description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    sale = models.BooleanField()
+    feature = models.BooleanField()
 
     def __str__(self):
         return str(self.product_name)
