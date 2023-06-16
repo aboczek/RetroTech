06757734027a88193dbcbd68fd7d72301690e84d
devon <adam.boczekwork@gmail.com>
2023-06-16 17:14:06 +0100

    Installing allauth + link urls + requirements

--- retroproject/settings.py
+++ retroproject/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'home',
     'items',
     'basket',
@@ -83,6 +87,14 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 WSGI_APPLICATION = 'retroproject.wsgi.application'
