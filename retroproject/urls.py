57d24487981c9eb95b1f99ca18bf150db6a0538d
devon <adam.boczekwork@gmail.com>
2023-06-17 12:49:54 +0100

    Setting allauth in settings + copy allauth tempalates to main tempalates

--- retroproject/urls.py
+++ retroproject/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('', include('items.urls')),
     path('', include('checkout.urls')),
