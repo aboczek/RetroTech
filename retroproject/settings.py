0f0873eb0f34b2af5f43bcbd279e91a400ee3953
devon <adam.boczekwork@gmail.com>
2023-06-19 20:29:22 +0100

    Making debug dynamic as per mentor advice

--- retroproject/settings.py
+++ retroproject/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['retrotech.herokuapp.com', '127.0.0.1']
 
