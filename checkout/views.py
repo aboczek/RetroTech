ac287beecfdbafb79b62f603c7d51c47fd84716b
devon <adam.boczekwork@gmail.com>
2023-07-06 19:41:18 +0100

    Adding js script + making it include + rendering keys in checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -17,6 +17,8 @@ def checkout(request):
     context = {
         'title': 'RetroTech Checkout!',
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NQwPPKXtaWpDhDxlnJtpmTVy5JrBP1o2lsH05CSKTpOvbQE1lbXMqTAE7rmpnmViowJkWBw7vX0T2mnccCe1DKG00mvjwQj8R',
+        'client_secret': 'testing',
     }
 
     return render(request, 'checkout/checkout.html', context)
