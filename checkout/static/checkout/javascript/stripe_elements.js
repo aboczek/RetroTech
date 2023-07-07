5f2647d00217e99fef8991d93a84358de67cfd4e
devon <adam.boczekwork@gmail.com>
2023-07-07 20:51:57 +0100

    Adding error message if card is invalid

--- checkout/static/checkout/javascript/stripe_elements.js
+++ checkout/static/checkout/javascript/stripe_elements.js
@@ -1,8 +1,8 @@
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
-var elements = stripe.elements();
-var style = {
+const stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+const client_secret = $('#id_client_secret').text().slice(1, -1);
+const stripe = Stripe(stripe_public_key);
+const elements = stripe.elements();
+const style = {
     base: {
         color: '#212529',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
@@ -17,7 +17,18 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var card = elements.create('card', {
+const card = elements.create('card', {
     style: style
 });
 card.mount('#card-element');
+
+// Handle for realtime validation errors on the card element.
+card.addEventListener('change', function (e) {
+    const errorDiv = document.querySelector('#card-errors');
+    if (e.error) {
+        const html = `*<span class="mt-2">${e.error.message}</span>`;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
