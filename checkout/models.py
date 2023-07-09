318efa6ff0008036bf2dc27e0d4ae4dda1a22430
devon <adam.boczekwork@gmail.com>
2023-07-09 15:28:36 +0100

    Adding models to home app for user profile

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from items.models import Item
+from home.models import UserProfile
 from django_countries.fields import CountryField
 
 
@@ -11,6 +12,9 @@ class Order(models.Model):
     Order model.
     """
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
     full_name = models.CharField(max_length=60, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
