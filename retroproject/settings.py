740d4e9a3919d9e3c16f6fe228c5f248cbc6e337
devon <adam.boczekwork@gmail.com>
2023-07-12 23:29:34 +0100

    testing

--- retroproject/settings.py
+++ retroproject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG')
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = '1'
 
 ALLOWED_HOSTS = ['retrotech.herokuapp.com', '127.0.0.1']
 
