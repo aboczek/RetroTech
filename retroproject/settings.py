5359e1e564c635a13f6befe7700e384cc80be0e2
devon <adam.boczekwork@gmail.com>
2023-07-13 00:05:26 +0100

    Still fighting sending email error

--- retroproject/settings.py
+++ retroproject/settings.py
@@ -210,7 +210,7 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USER_HOST = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
