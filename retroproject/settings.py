a07a2f1794f42936b0ab4de9fd438faeea875301
devon <adam.boczekwork@gmail.com>
2023-07-12 22:35:58 +0100

    Push for heroku again

--- retroproject/settings.py
+++ retroproject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG')
-DEBUG = '1'
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['retrotech.herokuapp.com', '127.0.0.1']
 
