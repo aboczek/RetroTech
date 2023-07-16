490c8c6e4d2c04fb0753e2dba34df0ad1dbd15c5
devon <adam.boczekwork@gmail.com>
2023-07-16 18:05:39 +0100

    Add signal if sale is true takes 30% of price down

---
+++ items/sale_signals.py
@@ -0,0 +1,13 @@
+from decimal import Decimal
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import Item
+
+
+@receiver(pre_save, sender=Item)
+def update_sale_price(sender, instance, **kwargs):
+    """
+    If Sale is True price of item will be reduced by 30%.
+    """
+    if instance.sale:
+        instance.price = instance.price * Decimal('0.7')
