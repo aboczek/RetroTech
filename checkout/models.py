df0ff47aa46b510430111a8b0da162e19a4fe2fb
devon <adam.boczekwork@gmail.com>
2023-07-17 23:36:31 +0100

    Added 404 and 500 templates

--- checkout/models.py
+++ checkout/models.py
@@ -82,4 +82,5 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU number {self.item.sku_number} on order {self.order.order_number}'
+        return f'SKU number {self.item.sku_number} \
+              on order {self.order.order_number}'
